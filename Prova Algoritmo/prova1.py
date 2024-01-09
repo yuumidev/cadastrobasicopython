9d9e78dfc56edce52aec309f322511b3849dca7b
Amanda <155692777+yuumizinea@users.noreply.github.com>
2024-01-08 21:46:35 -0300

    Add files via upload

---
+++ Prova Algoritmo/prova1.py
@@ -0,0 +1,17 @@
+empresa = "Clínica Ramos"
+cnpj = "98146779000152"
+ano_de_inicio = 2010
+aberta = True
+salario_medico = 10.000
+salario_enfermeira = 4.500
+salario_recep = 2.000
+
+
+nome = input("Digite o seu nome: ")
+cpf = input("Digite seu CPF: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite o seu peso: "))
+possui_seguro = True
+
+print(f"Bem Vindo(a) {nome}, agora você está cadastrado(a) no nosso plano médico. Seu CPF é {cpf}, você tem {idade} anos e {altura}cm de altura com o peso de {peso}kg. É um prazer ter você conosco.")
